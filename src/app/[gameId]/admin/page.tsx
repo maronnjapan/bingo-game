33dd53aff3a35de1501e2fc8cfbeaa56c14e24a2
maronnjapan <tihoutaikai20@gmail.com>
2024-12-12 00:54:37 +0900

    IDの取り方変更

--- src/app/[gameId]/admin/page.tsx
+++ src/app/[gameId]/admin/page.tsx
@@ -3,6 +3,7 @@ import { BingoDashboard } from "./_components/BingoDashboard";
 
 
 
-export default function Page({ params }: { params: { gameId: string } }) {
-    return <BingoDashboard gameId={params.gameId}></BingoDashboard>
+export default async function Page({ params }: { params: Promise<{ gameId: string }> }) {
+    const gameId = (await params).gameId
+    return <BingoDashboard gameId={gameId}></BingoDashboard>
 }
--- src/app/[gameId]/page.tsx
+++ src/app/[gameId]/page.tsx
@@ -1,5 +1,6 @@
 import { BingoBoard } from "./_components/BingoBoard";
 
-export default function Home({ params }: { params: { gameId: string } }) {
-    return <BingoBoard gameId={params.gameId} />
+export default async function Home({ params }: { params: Promise<{ gameId: string }> }) {
+    const gameId = (await params).gameId
+    return <BingoBoard gameId={gameId} />
 }
